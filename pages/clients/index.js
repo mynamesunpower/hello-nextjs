02530363925e8f9c349bcff42179876e24306a20
Taeyang Kim <mynamesunpower@gmail.com>
2021-09-10 16:36:22 +0900

    routing with next/router custom 404 page

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,26 @@
+import Link from "next/link";
 export default function ClientsPage() {
+  const clients = [
+    { id: "max", name: "maximilian" },
+    { id: "sunny", name: "sunnykim" },
+  ];
   return (
     <div>
       <h1>The Clients page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 }
